1c803bc5acdfde46b30f091f4f798b2130279deb
Anusha-ch123 <anushachilka99@gmail.com>
2025-06-21 20:38:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let demo = window.prompt("Enter a Number:")
+if(demo > 0){
+    document.writeln("The number is Positive.");
+}
+else if(demo < 0){
+    document.writeln("The number is Negative.");
+}
+else{
+    document.writeln("The number is Zero.");
+}
